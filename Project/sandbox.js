7f40bcdf5d21a01d0ed0b7151db5c32599875c9e
TechWithTee <68128807+taiwoajasa245@users.noreply.github.com>
2022-12-24 17:19:10 +0000

    Update sandbox.js

--- Project/sandbox.js
+++ Project/sandbox.js
@@ -11,7 +11,7 @@ let num = document.querySelector(".num");
 function myIncrement(){ 
     let num = document.querySelector(".num"); 
     // adding one after every click
-    count += 1; 
+    count++; 
     num.innerText = count;  
 }
 
@@ -23,9 +23,63 @@ function save(){
     count = 0; 
     num.innerText = 0; 
 }
+function startTimer(){
+    const timeLimit = document.getElementById("timer-input").value; 
+
+    // convert the time limit ot seconds 
+
+    const timeLimitInSeconds = timeLimit * 30; 
+
+    // set the initial timer value
+    let timeRemaining = timeLimitInSeconds; 
+
+
+
+    //update the timer display every second
+    const timerInterval = setInterval( () => { 
+        // decrement the time remaining by one second 
+        timeRemaining--; 
+
+        // update the timer display 
+        const time = document.getElementById("timer-display");
+
+        time.innerHTML = `Time Remaining: ${timeRemaining} seconds`; 
+
+        // set the color of the timer when it is in 29
+        
+        if (timeRemaining < 30) {
+            time.style.color = "green"; 
+        }
+        
+        // set the color of the timer when reached 20 
+        if (timeRemaining < 21) {
+            time.style.color = "yellow"; 
+        }
+        
+        if (timeRemaining < 11) {
+            time.style.color = "red"; 
+           
+        }
+    
+        // if the time remaining is zero , clear the interval and stop the timer
+        if (timeRemaining === 0){ 
+            clearInterval(timerInterval); 
+            save()
+            alert("Time is Over"); 
+        }
+
+    }, 1000); 
+
+}; 
+
+const timerForm = document.getElementById('timer-form');
+timerForm.addEventListener('submit', e => { 
+    e.preventDefault(); 
+    startTimer(); 
+}); 
 
 function myDelete() {
-    count -= 1; 
+    count--; 
     num.textContent = count;
     let i = count; 
     // making sure the result is a positive value
